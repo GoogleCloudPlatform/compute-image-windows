d05770892c8af014a1d62ce6881e3348ff692ae8
Andrew Jackura <ajackura@google.com>
2016-11-08 13:17:34 -0800

    Fix GCEMetadataScripts to run as 64bit (#80)

--- metadata_scripts/run_startup_scripts.cmd
+++ metadata_scripts/run_startup_scripts.cmd
@@ -19,4 +19,4 @@ REM A scheduled task may only run for up to three days before termination.
 REM We execute the startup script asynchronously so it may run without
 REM this three day maximum runtime limitation.
 
-start "C:\Program Files\Google\Compute Engine\metadata_scripts\GCEMetadataScripts.exe" "startup"
+start "" "C:\Program Files\Google\Compute Engine\metadata_scripts\GCEMetadataScripts.exe" "startup"
