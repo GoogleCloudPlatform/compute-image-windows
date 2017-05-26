76d49a7033fc830b9d80cf86f70538bddada860c
Andrew Jackura <ajackura@google.com>
2017-05-26 13:39:17 -0700

    update instance_setup.ps1 to not throw error if route exists

--- sysprep/instance_setup.ps1
+++ sysprep/instance_setup.ps1
@@ -122,7 +122,7 @@ function Change-InstanceProperties {
   }
   Write-Log 'MTU set to 1460.'
 
-  _RunExternalCMD route /p add 169.254.169.254 mask 255.255.255.255 0.0.0.0 if $netkvm[0].InterfaceIndex metric 1
+  _RunExternalCMD route /p add 169.254.169.254 mask 255.255.255.255 0.0.0.0 if $netkvm[0].InterfaceIndex metric 1 -ErrorAction SilentlyContinue
   Write-Log 'Added persistent route to metadata netblock via first netkvm adapter.'
 
   # Enable access to Windows administrative file share.
