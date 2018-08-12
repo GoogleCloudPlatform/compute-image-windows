c794d96f7b615941c43f468bceed036d20ce9c6a
Joseph Richey <joerichey94@gmail.com>
2018-08-11 18:30:59 -0700

    Allow building where GOOS is not Windows/Linux
    These changes make the stub files no longer be linux specific. It also
    changes agent_build.sh to no longer use GNU grep specific extensions.

--- agent_build.sh
+++ agent_build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-version=$(cat google-compute-engine-windows.goospec | grep -Po '"version":\s+"\K.+(?=",)')
+version=$(cat google-compute-engine-windows.goospec | sed -nE 's/.*"version":.*"(.+)".*/\1/p')
 if [[ $? -ne 0 ]]; then
   echo "could not match version in goospec"
   exit 1
