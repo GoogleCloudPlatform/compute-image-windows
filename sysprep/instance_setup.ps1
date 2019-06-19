9fb2c4fa3b7a021c74e518c7facd73a338d94d28
Peter Mounce <petermounce@improbable.io>
2019-06-19 07:40:36 +0100

    Optimize the startup of powershell sessions
    Before doing this, a powershell session takes around 6s to begin on an n1-standard-4, over winrm
    
    Afterwards, on the order of 200ms
    
    For us, using winrm as the packer/ansible communicator, this reduced provisioning time from around 25m to around 8m
    
    Not sure if this is the place to call it, compared to it happening inside an on-boot scheduled task; happy to move it, obviously.

--- sysprep/instance_setup.ps1
+++ sysprep/instance_setup.ps1
@@ -226,12 +226,12 @@ function Create-GCEStartup {
     .SYNOPSIS
       Setup the GCEStartup scheduled task.
     .DESCRIPTION
-      We have to use the API for this create call as schtasks.exe sets an 
+      We have to use the API for this create call as schtasks.exe sets an
       activation time of now for a task with the onstart schedule, this causes
       issues when the timezone is changed.
   #>
   $run_startup_scripts = "$script:gce_install_dir\metadata_scripts\run_startup_scripts.cmd"
-  
+
   $service = New-Object -ComObject("Schedule.Service")
   $service.Connect()
   $task = $service.NewTask(0)
@@ -243,6 +243,24 @@ function Create-GCEStartup {
   $trigger = $task.Triggers.Create(8)
   $folder = $service.GetFolder('\')
   $folder.RegisterTaskDefinition('GCEStartup',$task,6,'System',$null,5) | Out-Null
+}
+
+function Optimize-PowershellAssemblies {
+  # NGEN powershell assembly, improves startup time of powershell by 10x
+  $old_path = $env:path
+  try {
+    $env:path = [Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
+    [AppDomain]::CurrentDomain.GetAssemblies() | % {
+      if (! $_.location) {continue}
+      $Name = Split-Path $_.location -leaf
+      if ($Name.startswith("Microsoft.PowerShell.")) {
+        Write-Progress -Activity "Native Image Installation" -Status "$name"
+        ngen install $_.location | % {"`t$_"}
+      }
+    }
+  } finally {
+    $env:path = $old_path
+  }
 }
 
 # Check if COM1 exists.
@@ -287,6 +305,7 @@ else {
   Write-Log 'Running startup scripts from metadata server.'
   Create-GCEStartup
   Invoke-ExternalCommand schtasks /run /tn GCEStartup
+  Optimize-PowershellAssemblies
 
   Write-Log "Instance setup finished. $global:hostname is ready to use." -important
 
