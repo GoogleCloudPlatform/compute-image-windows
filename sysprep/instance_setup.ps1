924f30d7a3e4cb030e89d38efd68ef4fa4812f77
Peter Mounce <petermounce@improbable.io>
2019-06-25 23:17:18 +0100

    review feedback
    don't brutalise the PATH; move the call later in the sequence

--- sysprep/instance_setup.ps1
+++ sysprep/instance_setup.ps1
@@ -247,19 +247,17 @@ function Create-GCEStartup {
 
 function Optimize-PowershellAssemblies {
   # NGEN powershell assembly, improves startup time of powershell by 10x
-  $old_path = $env:path
+  pushd [Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
   try {
-    $env:path = [Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
     [AppDomain]::CurrentDomain.GetAssemblies() | % {
-      if (! $_.location) {continue}
-      $Name = Split-Path $_.location -leaf
-      if ($Name.startswith("Microsoft.PowerShell.")) {
-        Write-Progress -Activity "Native Image Installation" -Status "$name"
-        ngen install $_.location | % {"`t$_"}
-      }
+    if (! $_.location) {continue}
+    $Name = Split-Path $_.location -leaf
+    if ($Name.startswith("Microsoft.PowerShell.")) {
+      Write-Log "Native Image Installation: $name"
+      ngen install $_.location | % {"`t$_"}
     }
   } finally {
-    $env:path = $old_path
+    popd
   }
 }
 
@@ -305,7 +303,6 @@ else {
   Write-Log 'Running startup scripts from metadata server.'
   Create-GCEStartup
   Invoke-ExternalCommand schtasks /run /tn GCEStartup
-  Optimize-PowershellAssemblies
 
   Write-Log "Instance setup finished. $global:hostname is ready to use." -important
 
@@ -316,4 +313,5 @@ else {
   Wait-Job $activate_job | Receive-Job | ForEach-Object {
     Write-Log $_
   }
+  Optimize-PowershellAssemblies
 }
