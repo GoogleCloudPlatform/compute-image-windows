1b0671047aaff147924e153facad21a46bad539a
Liam Hopkins <liamh@google.com>
2019-06-30 01:03:46 -0700

    clock skew daemon

---
+++ GCEWindowsAgent/clock.go
@@ -0,0 +1,49 @@
+//  Copyright 2019 Google Inc. All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package main
+
+import (
+	"os/exec"
+	"runtime"
+)
+
+type clockskewMgr struct{}
+
+func (a *clockskewMgr) diff() bool {
+	return oldMetadata.Instance.VirtualClock.DriftToken != newMetadata.Instance.VirtualClock.DriftToken
+}
+
+func (a *clockskewMgr) timeout() bool {
+	return false
+}
+
+func (a *clockskewMgr) disabled() (disabled bool) {
+	enabled := config.Section("Daemons").Key("clock_skew_daemon").MustBool(true)
+	return runtime.GOOS == "windows" || !enabled
+}
+
+func (a *clockskewMgr) set() error {
+	if runtime.GOOS == "freebsd" {
+		err := exec.Command("service", "ntpd", "status").Run()
+		if err == nil {
+			defer exec.Command("service", "ntpd", "start").Run()
+			exec.Command("service", "ntpd", "stop").Run()
+		}
+		// TODO get server
+		return exec.Command("ntpdate", "metadata.google.internal").Run()
+	}
+
+	return exec.Command("/sbin/hwclock", "--hctosys").Run()
+}
