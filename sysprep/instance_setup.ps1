7c2b7ba4dcc6a563cd29fc95fe38e719f444a9e6
Lawrence Hwang <lawrencehwang@google.com>
2021-05-07 15:16:28 -0700

    Set OSConfig agent service during sysprep

--- sysprep/instance_setup.ps1
+++ sysprep/instance_setup.ps1
@@ -128,13 +128,13 @@ function Change-InstanceProperties {
   $nics | Invoke-CimMethod -Name SetDNSServerSearchOrder -Arguments @{DNSServerSearchOrder=$null}
   Write-Log 'All networks set to DHCP.'
 
-  $netkvm = Get-CimInstance Win32_NetworkAdapter -filter "ServiceName='netkvm'"	
-  $netkvm | ForEach-Object {	
-    Invoke-ExternalCommand netsh interface ipv4 set interface $_.NetConnectionID mtu=1460 | Out-Null	
-  }	
-  Write-Log 'MTU set to 1460.'	
+  $netkvm = Get-CimInstance Win32_NetworkAdapter -filter "ServiceName='netkvm'"
+  $netkvm | ForEach-Object {
+    Invoke-ExternalCommand netsh interface ipv4 set interface $_.NetConnectionID mtu=1460 | Out-Null
+  }
+  Write-Log 'MTU set to 1460.'
 
-  Invoke-ExternalCommand route /p add 169.254.169.254 mask 255.255.255.255 0.0.0.0 if $netkvm[0].InterfaceIndex metric 1 -ErrorAction SilentlyContinue	
+  Invoke-ExternalCommand route /p add 169.254.169.254 mask 255.255.255.255 0.0.0.0 if $netkvm[0].InterfaceIndex metric 1 -ErrorAction SilentlyContinue
   Write-Log 'Added persistent route to metadata netblock via first netkvm adapter.'
 }
 
@@ -220,6 +220,9 @@ if ($specialize) {
     Write-LogError
   }
 
+  Write-Log 'Enable google_osconfig_agent during the specialize configuration pass.'
+  Set-Service google_osconfig_agent -StartupType Automatic -Verbose -ErrorAction Continue
+
   Write-Log 'Finished with sysprep specialize phase, restarting...'
 }
 else {
@@ -236,4 +239,4 @@ else {
   & $script:activate_instance_script_loc | ForEach-Object {
     Write-Log $_
   }
-} 
+}
