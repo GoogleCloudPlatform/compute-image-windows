b8db74ad93472076c187aa34ac7df1d7d834aca7
Han Gao <hanga@google.com>
2021-05-19 21:39:18 +0000

    deprecate master branch

--- third_party/vss/GoogleVssProvider/uninstall_provider.bat
+++
@@ -1,3 +0,0 @@
-cscript.exe "c:\Program Files\Google\Compute Engine\vss\register_app.vbs" -unregister "Google Vss Provider"
-regsvr32 /s /u "C:\Program Files\Google\Compute Engine\vss\GoogleVssProvider.dll"
-
