df5b3c44749334d42c2f97e8e12b31244201636c
James Jerger <35977367+jjerger@users.noreply.github.com>
2023-09-29 15:41:41 -0700

    Move final error logic inside retry loop
    Better logging and actually retries 3 times

--- sysprep/activate_instance.ps1
+++ sysprep/activate_instance.ps1
@@ -85,12 +85,13 @@ function Activate-Instance {
       break
     }
     else {
-      Write-Output "Activation failed: Will try $retry_count more time(s)."
       $retry_count = $retry_count - 1
-    }
-
-    if ($retry_count -eq 0) {
-      Write-Output 'Max activation retry count reached: Giving up.'
+      if ($retry_count -eq 0) {
+        Write-Output 'ACtivation failed. Max activation retry count reached: Giving up.'
+      }
+      else {
+        Write-Output "Activation failed. Will try $retry_count more time(s)."
+      }
     }
   }
 }
