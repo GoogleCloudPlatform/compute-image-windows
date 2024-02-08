e89690cf1aedce0566502c8225f8e746a208576f
James Jerger <35977367+jjerger@users.noreply.github.com>
2024-02-07 22:06:20 -0800

    Switch from string matching to registry key value for activation status
    More deterministic; other method relies on system display language being English.

--- sysprep/activate_instance.ps1
+++ sysprep/activate_instance.ps1
@@ -267,17 +267,17 @@ function Verify-ActivationStatus {
   [String]$status = $null
 
   try {
-    $slmgr_status = & cscript //E:VBScript //nologo $env:windir\system32\slmgr.vbs /dli
+    $activation_status = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform\Activation" -Name ProductActivationResult).ProductActivationResult
   }
   catch {
     return $active
   }
 
-  $status = $slmgr_status | Select-String -Pattern '^License Status:'
-  # The initial space is to ensure "Unlicensed" does not match.
-  if ($status -match ' Licensed') {
+  # Anything other than 0x0 is a failure.
+  if ($activation_status -eq '0') {
     $active = $true
   }
+  Write-Host $activation_status
   return $active
 }
 
