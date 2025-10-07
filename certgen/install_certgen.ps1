06ddcea3f38806367563c00b1c27aed0949c3318
Hank Huang <theorem@google.com>
2025-10-07 20:12:25 +0000

    Added googet package and moved install scripts to respective directories.

---
+++ certgen/install_certgen.ps1
@@ -0,0 +1,51 @@
+#  Copyright 2025 Google Inc. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http:#www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+#Requires -RunAsAdministrator
+
+$ErrorActionPreference = 'Stop'
+
+# Get OS Version
+$OSVersion = [System.Environment]::OSVersion.Version
+$Major = $OSVersion.Major
+$Minor = $OSVersion.Minor
+
+# Windows Server 2008 is 6.0
+# Windows Server 2008 R2 is 6.1
+$IsWin2008Legacy = ($Major -eq 6 -and ($Minor -eq 0 -or $Minor -eq 1))
+
+# $PSScriptRoot is the directory where the script is running, inside the expanded GooGet package.
+$PackageRoot = $PSScriptRoot
+
+$InstallDir = "$env:ProgramFiles\Google\Compute Engine\tools"
+
+if (!(Test-Path $InstallDir)) {
+    New-Item -ItemType Directory -Path $InstallDir -Force
+}
+
+$TargetExePath = Join-Path $InstallDir "certgen.exe"
+
+if ($IsWin2008Legacy) {
+    Write-Host "Detected Windows version $Major.$Minor. Installing legacy certgen due to Go compatibility."
+    $SourceExe = Join-Path $PackageRoot "last_known_good\win_ver_6_1\certgen.exe"
+}
+else {
+    Write-Host "Detected modern Windows version. Installing current certgen."
+    $SourceExe = Join-Path $PackageRoot "certgen.exe"
+}
+
+Write-Host "Copying $SourceExe to $TargetExePath"
+Copy-Item -Path $SourceExe -Destination $TargetExePath -Force
+
+Write-Host "Certgen installation complete."
--- go.mod
+++ go.mod
@@ -5,18 +5,16 @@ go 1.25
 require (
 	github.com/GoogleCloudPlatform/compute-image-tools/daisy v0.0.0-20220307223029-04ad9f09e70d
 	github.com/hashicorp/packer-plugin-azure v1.4.5
-	google.golang.org/api v0.250.0
+	google.golang.org/api v0.252.0
 )
 
 require (
-	cloud.google.com/go/auth v0.16.5 // indirect
+	cloud.google.com/go/auth v0.17.0 // indirect
 	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
 	cloud.google.com/go/compute/metadata v0.9.0 // indirect
-	github.com/blang/semver v3.5.1+incompatible // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/google/googet/v2 v2.20.0 // indirect
 	github.com/google/s2a-go v0.1.9 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
@@ -34,7 +32,7 @@ require (
 	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
 	golang.org/x/time v0.13.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250929231259-57b25ae835d4 // indirect
-	google.golang.org/grpc v1.75.1 // indirect
-	google.golang.org/protobuf v1.36.9 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251006185510-65f7160b3a87 // indirect
+	google.golang.org/grpc v1.76.0 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
 )
